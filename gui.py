5eafecd3961dca915e1cf802b0d974782130945b
anirudhbhowmiknitk <anirudhbhowmik.251ee209@nitk.edu.in>
2026-06-03 11:28:12 +0530

    Initial Command

--- gui.py
+++ gui.py
@@ -1,95 +1,56 @@
 import FreeSimpleGUI as sg
 import functions
 
+
 label = sg.Text("Type in a to-do")
-
-input_box = sg.Input(
-    key="todo",
-    tooltip="Enter a todo"
-)
-
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
+
 exit_button = sg.Button("Exit")
 
-list_box = sg.Listbox(
-    values=functions.get_todos(),
-    key="todos",
-    enable_events=True,
-    size=(45, 10)
-)
-
-layout = [
-    [label],
-    [input_box, add_button],
-    [list_box],
-    [edit_button, complete_button],
-    [exit_button]
-]
-
-window = sg.Window(
-    "My To-Do App",
-    layout=layout,
-    font=("Helvetica", 20)
-)
+window = sg.Window("My To-Do App",
+                   layout=[
+                       [label],
+                       [input_box, add_button],
+                       [list_box, edit_button, complete_button],
+                       [exit_button],
+                   ],
+                   font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
-
-    if event in (sg.WIN_CLOSED, "Exit"):
-        break
-
-    if event == "Add":
-        todo = values["todo"].strip()
-
-        if todo:
+    match event:
+        case "Add":
             todos = functions.get_todos()
-            todos.append(todo + "\n")
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
 
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
             functions.write_todos(todos)
 
             window["todos"].update(values=todos)
-            window["todo"].update("")
-
-    elif event == "Edit":
-
-        if not values["todos"]:
-            sg.popup("Please select a todo first.")
-            continue
-
-        selected_todo = values["todos"][0]
-        new_todo = values["todo"].strip()
-
-        todos = functions.get_todos()
-
-        index = todos.index(selected_todo)
-        todos[index] = new_todo + "\n"
-
-        functions.write_todos(todos)
-
-        window["todos"].update(values=todos)
-
-    elif event == "Complete":
-
-        if not values["todos"]:
-            sg.popup("Please select a todo first.")
-            continue
-
-        selected_todo = values["todos"][0]
-
-        todos = functions.get_todos()
-
-        todos.remove(selected_todo)
-
-        functions.write_todos(todos)
-
-        window["todos"].update(values=todos)
-        window["todo"].update("")
-
-    elif event == "todos":
-
-        if values["todos"]:
-            window["todo"].update(values["todos"][0].strip())
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
